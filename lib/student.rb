6de74e465075fb71ec515349e21c6e3bd9e09262
Eric McKinney <mckinney99@gmail.com>
2017-12-06 05:39:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
     FROM students
     WHERE name = ?
     SQL
-    row = DB[:conn].execute(sql, name)[1]
+    row = DB[:conn].execute(sql, name)[2]
     student.new(row[0], row[1], row[2])
   end
 
